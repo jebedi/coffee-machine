ea82253d5c0a3905fe2c38f1ee4c622e6b3de2ca
yendrvalde <jotbed@gmail.com>
2020-07-03 17:56:14 +0100

    refactored actions into enum type

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -76,6 +76,64 @@ enum CoffeeType {
     public abstract int getPrice();
 }
 
+enum ActionType {
+    BUY {
+        @Override
+        public void performAction(CoffeeMachine machine, Scanner scanner) {
+            System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+            String coffeeChoice = scanner.next();
+            switch (coffeeChoice) {
+                case "1":
+                    machine.prepareCoffee(CoffeeType.ESPRESSO);
+                    break;
+                case "2":
+                    machine.prepareCoffee(CoffeeType.LATTE);
+                    break;
+                case "3":
+                    machine.prepareCoffee(CoffeeType.CAPPUCCINO);
+                    break;
+                case "back":
+                    break;
+            }
+        }
+    }, FILL {
+        @Override
+        public void performAction(CoffeeMachine machine, Scanner scanner) {
+            System.out.println("Write how many ml of water do you want to add: ");
+            machine.setWaterReserves(machine.getWaterReserves() + scanner.nextInt());
+            System.out.println("Write how many ml of milk do you want to add: ");
+            machine.setMilkReserves(machine.getMilkReserves() + scanner.nextInt());
+            System.out.println("Write how many grams of coffee beans do you want to add: ");
+            machine.setCoffeeReserves(machine.getCoffeeReserves() + scanner.nextInt());
+            System.out.println("Write how many disposable cups of coffee do you want to add: ");
+            machine.setCupReserves(machine.getCupReserves() + scanner.nextInt());
+        }
+    }, TAKE {
+        @Override
+        public void performAction(CoffeeMachine machine, Scanner scanner) {
+            System.out.println("I gave you " + machine.getMoneyReserves());
+            machine.setMoneyReserves(0);
+        }
+    }, EXIT {
+        @Override
+        public void performAction(CoffeeMachine machine, Scanner scanner) {
+            machine.turnOffMachine();
+        }
+    }, REMAINING {
+        @Override
+        public void performAction(CoffeeMachine machine, Scanner scanner) {
+            System.out.println("The coffee machine has:");
+            System.out.println(machine.getWaterReserves() + " of water");
+            System.out.println(machine.getMilkReserves() + " of milk");
+            System.out.println(machine.getCoffeeReserves() + " of coffee beans");
+            System.out.println(machine.getCupReserves() + " of disposable cups");
+            System.out.println(machine.getMoneyReserves() + " of money");
+        }
+    };
+
+    public abstract void performAction(CoffeeMachine machine, Scanner scanner);
+}
+
 public class CoffeeMachine {
 
     private static final String BUY = "buy";
@@ -83,13 +141,57 @@ public class CoffeeMachine {
     private static final String TAKE = "take";
     private static final String EXIT = "exit";
     private static final String REMAINING = "remaining";
-    private static final String BACK = "back";
+
     private int waterReserves;
     private int milkReserves;
     private int coffeeReserves;
     private int cupReserves;
     private int moneyReserves;
     private boolean turnedOn = true;
+
+    public void setWaterReserves(int waterReserves) {
+        this.waterReserves = waterReserves;
+    }
+
+    public void setMilkReserves(int milkReserves) {
+        this.milkReserves = milkReserves;
+    }
+
+    public void setCoffeeReserves(int coffeeReserves) {
+        this.coffeeReserves = coffeeReserves;
+    }
+
+    public void setCupReserves(int cupReserves) {
+        this.cupReserves = cupReserves;
+    }
+
+    public void setMoneyReserves(int moneyReserves) {
+        this.moneyReserves = moneyReserves;
+    }
+    
+    public int getWaterReserves() {
+        return waterReserves;
+    }
+
+    public int getMilkReserves() {
+        return milkReserves;
+    }
+
+    public int getCoffeeReserves() {
+        return coffeeReserves;
+    }
+
+    public int getCupReserves() {
+        return cupReserves;
+    }
+
+    public int getMoneyReserves() {
+        return moneyReserves;
+    }
+
+    public boolean isTurnedOn() {
+        return turnedOn;
+    }
 
     public CoffeeMachine(int waterReserves, int milkReserves, int coffeeReserves, int cupReserves, int moneyReserves) {
         this.waterReserves = waterReserves;
@@ -104,7 +206,7 @@ public class CoffeeMachine {
         Scanner scanner = new Scanner(System.in);
         CoffeeMachine machine = new CoffeeMachine(400, 540, 120, 9, 550);
 
-        while (machine.turnedOn) {
+        while (machine.isTurnedOn()) {
             System.out.println("Write action (buy, fill, take, remaining, exit):");
             machine.processAction(scanner);
         }
@@ -113,38 +215,29 @@ public class CoffeeMachine {
     private void processAction(Scanner scanner) {
         switch (scanner.nextLine()) {
             case BUY:
-                processBuyAction(scanner);
+                ActionType.BUY.performAction(this, scanner);
                 break;
             case FILL:
-                processFillAction(scanner);
+                ActionType.FILL.performAction(this, scanner);
                 break;
             case TAKE:
-                processTakeAction(scanner);
+                ActionType.TAKE.performAction(this, scanner);
                 break;
             case REMAINING:
-                processRemainingAction();
+                ActionType.REMAINING.performAction(this, scanner);
                 break;
             case EXIT:
-                turnOffMachine();
+                ActionType.EXIT.performAction(this, scanner);
                 break;
         }
     }
 
-    private void turnOffMachine() {
+    public void turnOffMachine() {
         this.turnedOn = false;
     }
 
-    private void processRemainingAction() {
-        printStatus();
-    }
-
-    private void processTakeAction(Scanner scanner) {
-        System.out.println("I gave you " + this.moneyReserves);
-        moneyReserves = 0;
-    }
-
-    private void prepareCoffee(CoffeeType coffeeType) {
-        if (isEnoughResources(coffeeType)) {
+    public void prepareCoffee(CoffeeType coffeeType) {
+        if (hasEnoughResources(coffeeType)) {
             this.waterReserves -= coffeeType.getWaterNeeded();
             this.milkReserves -= coffeeType.getMilkNeeded();
             this.coffeeReserves -= coffeeType.getCoffeeNeeded();
@@ -153,52 +246,13 @@ public class CoffeeMachine {
         } else {
             System.out.println("Sorry, not enough resources!");
         }
-
     }
 
-    private boolean isEnoughResources(CoffeeType coffeeType) {
-        return this.waterReserves  >=  coffeeType.getWaterNeeded()  &&
-               this.milkReserves   >=  coffeeType.getMilkNeeded()   &&
-               this.coffeeReserves >=  coffeeType.getCoffeeNeeded() &&
-               this.cupReserves    >=  1;
+    private boolean hasEnoughResources(CoffeeType coffeeType) {
+        return this.waterReserves >= coffeeType.getWaterNeeded() &&
+                this.milkReserves >= coffeeType.getMilkNeeded() &&
+                this.coffeeReserves >= coffeeType.getCoffeeNeeded() &&
+                this.cupReserves >= 1;
     }
 
-
-    private void processFillAction(Scanner scanner) {
-        System.out.println("Write how many ml of water do you want to add: ");
-        this.waterReserves += scanner.nextInt();
-        System.out.println("Write how many ml of milk do you want to add: ");
-        this.milkReserves += scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans do you want to add: ");
-        this.coffeeReserves += scanner.nextInt();
-        System.out.println("Write how many disposable cups of coffee do you want to add: ");
-        this.cupReserves += scanner.nextInt();
-    }
-
-    private void processBuyAction(Scanner scanner) {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-        String coffeeChoice = scanner.next();
-        switch (coffeeChoice) {
-            case "1":
-                prepareCoffee(CoffeeType.ESPRESSO);
-                break;
-            case "2":
-                prepareCoffee(CoffeeType.LATTE);
-                break;
-            case "3":
-                prepareCoffee(CoffeeType.CAPPUCCINO);
-                break;
-            case "back":
-                break;
-        }
-    }
-
-    private void printStatus() {
-        System.out.println("The coffee machine has:");
-        System.out.println(waterReserves + " of water");
-        System.out.println(milkReserves + " of milk");
-        System.out.println(coffeeReserves + " of coffee beans");
-        System.out.println(cupReserves + " of disposable cups");
-        System.out.println(moneyReserves + " of money");
-    }
 }
